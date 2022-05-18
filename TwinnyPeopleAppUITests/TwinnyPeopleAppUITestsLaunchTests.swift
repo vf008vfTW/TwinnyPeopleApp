4535920b10d19db39c2071b08bec8c8e47b1d68d
vf008vf <vf008vf@twinny.co.kr>
2022-05-18 11:55:40 +0900

    Initial Commit

---
+++ TwinnyPeopleAppUITests/TwinnyPeopleAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TwinnyPeopleAppUITestsLaunchTests.swift
+//  TwinnyPeopleAppUITests
+//
+//  Created by 트위니 on 2022/05/18.
+//
+
+import XCTest
+
+class TwinnyPeopleAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

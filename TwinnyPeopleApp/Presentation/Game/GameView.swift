c550dd9f4e636d6e4aefbc608d39c8f8467cba64
vf008vf <vf008vf@twinny.co.kr>
2022-05-18 14:28:20 +0900

    Add HomeTabBarController
    HomeTabBarController를 추가했습니다.
    
    - HomeTabBarController 추가
    
    RPD-0003

---
+++ TwinnyPeopleApp/Presentation/Game/GameView.swift
@@ -0,0 +1,8 @@
+//
+//  GameView.swift
+//  TwinnyPeopleApp
+//
+//  Created by 트위니 on 2022/05/18.
+//
+
+import Foundation
---
+++ TwinnyPeopleApp/Presentation/HomeTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeTabBarController.swift
+//  TwinnyPeopleApp
+//
+//  Created by 트위니 on 2022/05/18.
+//
+
+import UIKit
+
+final class HomeTabBarController: UITabBarController {
+    
+    let gameView = GameViewController()
+    let summaryView = SummaryViewController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.viewControllers = [
+            tabBarConfig(rootViewController: gameView, tabBarImage: UIImage(systemName: "gamecontroller.fill")!),
+            tabBarConfig(rootViewController: summaryView, tabBarImage: UIImage(systemName: "note.text")!)
+        ]
+        tabBar.barTintColor = .clear
+        tabBar.unselectedItemTintColor = .white
+        tabBar.tintColor = .green
+    }
+
+    private func tabBarConfig(rootViewController: UIViewController, tabBarImage: UIImage) -> UINavigationController {
+        let rootViewController = rootViewController
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.tabBarItem.image = tabBarImage
+        navigationController.navigationBar.barTintColor = .systemGray4
+        return navigationController
+    }
+}
